f216724ec9968e1b58b3468ba1fbd8f32ecae718
Adan.Reyes <adancatari3@gmail.com>
2021-12-24 15:11:11 -0300

    eleminacion de archivos y modificacion en el nuevo carrusel de index.html

--- js/app.js
+++ js/app.js
@@ -1,8 +1,28 @@
 window.addEventListener('load', function(){
-    new Glider(this.document.querySelector('.carousel__lista'), {
+    new Glider(document.querySelector('.carousel__lista'), {
         slidesToScroll: 1,
-        slidesToShow: 6,
+        slidesToShow: 1,
         draggable: true,
-        dots: '.carousel__indicadores'
-    });
+        dots: '.carousel__indicadores',
+        
+        responsive: [
+            {
+              // screens greater than >= 775px
+                breakpoint: 775,
+                settings: {
+                    // Set to `auto` and provide item width to adjust to viewport
+                    slidesToShow: '2',
+                    slidesToScroll: '2',
+                }
+            },
+            {
+              // screens greater than >= 1024px
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                }
+            }
+            ]
+        });
 });
